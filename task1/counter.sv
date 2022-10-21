37cb2f9e90c3bf4b6e9ef76f58e652db6130e333
kushk2 <105926115+kushk2@users.noreply.github.com>
2022-10-21 11:44:42 +0100

    update

--- task1/counter.sv
+++ task1/counter.sv
@@ -5,11 +5,11 @@ module counter #(
     input logic             clk,    //clock
     input logic             rst,    //reset
     input logic             en,     //counter enable
-    input logic [WIDTH 1:0] count   //count output              
-)
+    input logic [WIDTH-1:0] count   //count output              
+);
 
 always_ff @ (posedge clk)
     if (rst)    count <= {WIDTH{1'b0}};
-    else        count <= count + {{WIDTH - 1{1'b0}}, en}
+    else        count <= count + {{WIDTH - 1{1'b0}}, en};
 
 endmodule
