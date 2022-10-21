34b33c7e628919476c49e957ec95c0b4bc69ff42
kushk2 <105926115+kushk2@users.noreply.github.com>
2022-10-21 15:42:27 +0100

    updated counter_tb.cpp

--- task1/counter_tb.cpp
+++ task1/counter_tb.cpp
@@ -25,7 +25,7 @@ int main(int argc, char **argv, char **env){
         
         //dump variables into VCD file and toggle clock
         for(clk = 0; clk < 2; clk++){
-            tfp->dump(2*i+clk);
+            tfp->dump(2*i+clk); //unit is in ps
             top->clk = !top->clk;
             top->eval ();
         }
